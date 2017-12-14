336ff354170dc8ba5f36099bb5623ec19d1cf79f
Liu Yang <liuyang@waijule.com>
2017-12-14 11:13:38 +0800

    33

---
+++ problems/33.Search.in.Rotated.Sorted.Array.js
@@ -0,0 +1,14 @@
+const search = (numbers, target) => {
+  if (target >= numbers[0]) {
+    for (let i = 0; i < numbers.length; i += 1) {
+      if (target === numbers[i]) return i;
+      if (numbers[i] < numbers[0]) return -1;
+    }
+  } else {
+    for (let i = numbers.length - 1; i > 0; i -= 1) {
+      if (target === numbers[i]) return i;
+      if (numbers[i] > numbers[0]) return -1;
+    }
+  }
+  return -1;
+};
