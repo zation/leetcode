dfca72c61ee5adcfca3e7201dfee91db7a955bf0
Zation <zation1@gmail.com>
2019-03-04 15:49:32 +0800

    Update 8.String-to-Integer-(atoi).js

--- problems/8.String-to-Integer-(atoi).js
+++ problems/8.String-to-Integer-(atoi).js
@@ -1,41 +1,38 @@
-// eslint-disable-next-line
-const myAtoi = (s) => {
-  let sign = 1;
-  let resultString = '';
-  let i = 0;
-  while (s[i] === ' ' && i < s.length) {
-    i += 1;
-  }
-  if (s[i] === '-') {
-    sign = -1;
-    i += 1;
-  } else if (s[i] === '+') {
-    i += 1;
-  }
-  while (i < s.length) {
-    if (resultString === '' && s[i] === '0') {
-      i += 1;
-      continue;
+/**
+ * @param {string} str
+ * @return {number}
+ */
+const MAX_INT = Math.pow(2, 31) - 1;
+const MIN_INT = Math.pow(-2, 31);
+const MAX_INT_PRE_CHECK = Math.floor(MAX_INT / 10);
+const MIN_INT_PRE_CHECK = Math.ceil(MIN_INT / 10);
+var myAtoi = function(str) {
+    let isPositive = true;
+    let result = 0;
+    let i = 0;
+    while(str[i] === ' ') {
+        i += 1;
     }
-
-    if (!s[i].match(/\d/)) {
-      return resultString === '' ? 0 : sign * Number(resultString);
+    if (str[i] === '-') {
+        isPositive = false;
+        i += 1;
+    } else if (str[i] === '+') {
+        i += 1;
     }
-
-    if (
-      Number(resultString) > 214748364 || (
-        Number(resultString) === 214748364 &&
-        (sign > 0 ? Number(s[i]) > 7 : Number(s[i]) > 8)
-      )
-    ) {
-      return sign > 0 ? 2147483647 : -2147483648;
+    while(i < str.length) {
+        const item = str[i];
+        const number = Number(item);
+        if (item === ' ' || Number.isNaN(number)) {
+            return result;
+        }
+        if (isPositive && (result > MAX_INT_PRE_CHECK || (result === MAX_INT_PRE_CHECK && number > 7))) {
+            return MAX_INT;
+        }
+        if (!isPositive && (result < MIN_INT_PRE_CHECK || (result === MIN_INT_PRE_CHECK && number > 8))) {
+            return MIN_INT;
+        }
+        result = isPositive ? result * 10 + number : result * 10 - number;
+        i += 1;
     }
-
-    resultString += s[i];
-    i += 1;
-  }
-
-  return sign * Number(resultString);
+    return result;
 };
-
-console.log(myAtoi('  -0012a42'));
