05e644f9ea70e8163ece917d681c9696064d14a2
Zation <zation1@gmail.com>
2019-03-04 15:18:51 +0800

    Update 7.Reverse-Integer.js

--- problems/7.Reverse-Integer.js
+++ problems/7.Reverse-Integer.js
@@ -1,18 +1,17 @@
-// eslint-disable-next-line
-const reverse = (number) => {
-  const numberString = number >= 0 ? number.toString() : (-number).toString();
-  let resultString = '';
-  for (let i = 0; i < numberString.length; i += 1) {
-    const currentNumberString = numberString[numberString.length - i - 1];
-    if (
-      Number(resultString) > 214748364 || (
-        Number(resultString) === 214748364 &&
-        (number > 0 ? Number(currentNumberString) > 7 : Number(currentNumberString) > 8)
-      )
-    ) {
-      return 0;
+/**
+ * @param {number} x
+ * @return {number}
+ */
+const MAX_INT = Math.pow(2, 31) - 1;
+const MIN_INT = Math.pow(-2, 31);
+var reverse = function(x) {
+    let result = 0;
+    while (x!=0) {
+        const pop = x % 10;
+        x = x > 0 ? Math.floor(x / 10) : Math.ceil(x / 10);
+        if (result > MAX_INT / 10 || (result === MAX_INT / 10 && pop > 7)) return 0;
+        if (result < MIN_INT / 10 || (result === MIN_INT / 10 && pop < -8)) return 0;
+        result = result * 10 + pop;
     }
-    resultString += currentNumberString;
-  }
-  return number >= 0 ? Number(resultString) : -Number(resultString);
+    return result;
 };
