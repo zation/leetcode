40d2591df39fe91f19cf9fbfb8afb3ab5775a545
Zation <zation1@gmail.com>
2019-03-04 14:59:10 +0800

    Update 5.Longest-Palindromic-Substring.js

--- problems/5.Longest-Palindromic-Substring.js
+++ problems/5.Longest-Palindromic-Substring.js
@@ -1,34 +1,32 @@
-// eslint-disable-next-line
-const longestPalindrome = (s) => {
-  if (!s || s.length === 1) return s;
-  let result = s[0];
-  let temp;
-  let left;
-  let right;
-  let i = 0;
-  while (i < s.length) {
-    temp = s[i];
-    right = i + 1;
-    left = i - 1;
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const getStartAndEnd = (s, start, end) => {
+    while(start >= 0 && end <s.length && s[start] === s[end]) {
+        start -= 1;
+        end += 1;
+    }
+    return [start + 1, end - 1];
+}
 
-    while (s[i] === s[right]) {
-      temp += s[right];
-      right += 1;
-      i += 1;
+var longestPalindrome = function(s) {
+    if (!s || s.length === 0) {
+        return '';
     }
-    while (right < s.length && left >= 0) {
-      if (s[left] === s[right]) {
-        temp = s[left] + temp + s[right];
-        left -= 1;
-        right += 1;
-      } else {
-        break;
-      }
+    let start = 0;
+    let end = 0;
+    for (let i=0; i<s.length; i+=1) {
+        const [start1, end1] = getStartAndEnd(s, i, i);
+        const [start2, end2] = getStartAndEnd(s, i, i + 1);
+        if (end1 - start1 >= end2 - start2 && end1 - start1 > end - start) {
+            end = end1;
+            start = start1;
+        } else if (end2 - start2 > end1 - start1 && end2 - start2 > end - start) {
+            end = end2;
+            start = start2;
+        }
+        console.log(i, end, end1);
     }
-    if (temp.length > result.length) {
-      result = temp;
-    }
-    i += 1;
-  }
-  return result;
+    return s.substring(start, end + 1);
 };
