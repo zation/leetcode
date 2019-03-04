42b9108bd22d9554958b61bfc8ba0071520dc147
Zation <zation1@gmail.com>
2019-03-04 12:02:49 +0800

    use js Map

--- problems/1.Two-Sum.js
+++ problems/1.Two-Sum.js
@@ -1,14 +1,11 @@
 // eslint-disable-next-line
-const twoSum = (numbers, target) => {
-  const map = {};
-  for (let i = 0; i < numbers.length; i += 1) {
-    const number = numbers[i];
-    const anotherNumber = target - number;
-    const anotherNumberIndex = map[anotherNumber.toString()];
-    if (anotherNumberIndex >= 0) {
-      return [anotherNumberIndex, i];
+const twoSum = function(nums, target) {
+    const map = new Map();
+    for(let i=0; i<nums.length; i++) {
+        const number = nums[i];
+        if (map.has(number)) {
+            return [map.get(number), i];
+        }
+        map.set(target - number, i);
     }
-    map[number.toString()] = i;
-  }
-  return [];
 };
