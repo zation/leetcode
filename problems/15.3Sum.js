c99220e7fae959c96f7eea77426de89a5ef907ff
Zation <zation1@gmail.com>
2019-03-05 14:33:45 +0800

    Update 15.3Sum.js

--- problems/15.3Sum.js
+++ problems/15.3Sum.js
@@ -1,47 +1,39 @@
-// eslint-disable-next-line
-const threeSum = (numbers) => {
-  const results = [];
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 
-  const sortedNumbers = numbers.sort((a, b) => a - b);
-  const map = new Map();
-  const sortedFilteredNumbers = [];
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (sortedNumbers[i] === sortedFilteredNumbers[sortedFilteredNumbers.length - 2] &&
-      sortedNumbers[i] === 0 &&
-      results.length === 0) {
-      results.push([0, 0, 0]);
-    } else if (sortedNumbers[i] !== sortedFilteredNumbers[sortedFilteredNumbers.length - 2]) {
-      sortedFilteredNumbers.push(sortedNumbers[i]);
-      map.set(sortedNumbers[i], sortedFilteredNumbers.length - 1);
-    }
+const threeSum = function (nums) {
+  if (!nums || nums.length === 0) {
+    return [];
   }
+  const sortedNums = nums.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < sortedNums.length; i += 1) {
+    let start = i + 1;
+    let end = sortedNums.length - 1;
+    const sum = -sortedNums[i];
+    while (start < end) {
+      const startNumber = sortedNums[start];
+      const endNumber = sortedNums[end];
+      if (startNumber + endNumber < sum) {
+        start += 1;
+      } else if (startNumber + endNumber > sum) {
+        end -= 1;
+      } else {
+        result.push([-sum, startNumber, endNumber]);
 
-  for (let i = 0; i < sortedFilteredNumbers.length - 1; i += 1) {
-    if (sortedFilteredNumbers[i] +
-      sortedFilteredNumbers[sortedFilteredNumbers.length - 2] +
-      sortedFilteredNumbers[sortedFilteredNumbers.length - 1] < 0 ||
-      sortedFilteredNumbers[i] +
-      sortedFilteredNumbers[i + 1] +
-      sortedFilteredNumbers[i + 2] > 0) {
-      continue;
-    }
-    for (let j = i + 1; j < numbers.length; j += 1) {
-      if (map.get(0 - sortedFilteredNumbers[i] - sortedFilteredNumbers[j]) > j) {
-        results.push([
-          sortedFilteredNumbers[i],
-          sortedFilteredNumbers[j],
-          0 - sortedFilteredNumbers[i] - sortedFilteredNumbers[j],
-        ]);
-      }
-      while (sortedFilteredNumbers[j] === sortedFilteredNumbers[j + 1] && j < numbers.length) {
-        j += 1;
+        while (start < end && startNumber === sortedNums[start]) {
+          start += 1;
+        }
+        while (start < end && endNumber === sortedNums[end]) {
+          end -= 1;
+        }
       }
     }
-    while (sortedFilteredNumbers[i] === sortedFilteredNumbers[i + 1] &&
-    i < sortedFilteredNumbers.length - 1) {
+    while (sortedNums[i] === sortedNums[i + 1]) {
       i += 1;
     }
   }
-
-  return results;
+  return result;
 };
